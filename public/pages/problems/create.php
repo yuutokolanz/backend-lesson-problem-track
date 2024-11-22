6e6382b938d443ffe3197a3948eb7da211616b41
Raul Ferreira da Roha <106343333+yuutokolanz@users.noreply.github.com>
2024-11-22 19:20:54 -0300

    Refactor to OO (#15)
    * Refactor create to OO without validations yet
    
    * Refactor create validations to OO
    
    * Setup error handler
    
    * Refactor retrive to OO
    
    * Refactor retrive to OO fix
    
    * Refactor edit and update to OO
    
    * Refactor destroy to OO

--- public/pages/problems/create.php
+++ public/pages/problems/create.php
@@ -1,5 +1,7 @@
 <?php
 
+require '/var/www/app/models/Problem.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method !== 'POST') {
@@ -7,20 +9,14 @@ if ($method !== 'POST') {
     exit;
 }
 
-$problem = $_POST['problem'];
-$title = trim($problem['title']);
-
 $errors = [];
 
-if (empty($title))
-    $errors['title'] = 'Não pode ser vazio';
+$params = $_POST['problem'];
+$problem = new Problem(title: $params['title']);
 
-if (empty($errors)){
-    define('DB_PATH', '/var/www/database/problems.txt');
-    file_put_contents(DB_PATH, $title . PHP_EOL, FILE_APPEND);
-    
+if ($problem->save()){
     header('Location: /pages/problems/');
-} else{
+} else {
     $title = 'Novo Problema';
     $view = '/var/www/app/views/problems/new.phtml';
 

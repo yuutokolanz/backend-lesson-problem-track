0667ee95e5f0fb5bb1ff0705f50f35a94f78b7ff
Raul F Rocha <raulferreiradarocha1@hotmail.com>
2024-11-23 19:27:24 -0300

    Refaction core env to OO

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,14 +1,12 @@
 <?php
 
 use Core\Constants\Constants;
+use Core\Env\EnvLoader;
 use Core\Errors\ErrorsHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 ErrorsHandler::init();
+EnvLoader::init();
 
-Constants::rootPath();
-
-require_once Constants::rootPath() . '/core/Env/env.php';
 // require_once ROOT_PATH . '/core/Debug/functions.php';
-// // require_once ROOT_PATH . '/core/errors/handler.php';
